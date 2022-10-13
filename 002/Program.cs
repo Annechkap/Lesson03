d16edc751b5ea9e670d9159a708f87dbb3322e01
Anna <annechkap@list.ru>
2022-10-13 23:47:12 +0300

    Добавлена задача 002

---
+++ 002/Program.cs
@@ -0,0 +1,31 @@
+﻿// 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+const int X = 0;
+const int Y = 1;
+const int C = 2;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] InputPoint(int point)
+{
+    int[] answer = new int[3];
+    answer[X] = Prompt($"Введите x {point}  ");
+    answer[Y] = Prompt($"Введите y {point}  ");
+    answer[C] = Prompt($"Введите c {point}  ");
+    return answer;
+}
+double Power2(int arg)
+{
+    return arg * arg;
+}
+int[] p1 = InputPoint(1);
+int[] p2 = InputPoint(2);
+
+double lenght = Math.Sqrt(Power2(p1[X] - p2[X]) + Power2(p1[Y] - p2[Y]) + Power2(p1[C] - p2[C]));
+
+System.Console.WriteLine($"{lenght:f2}");
